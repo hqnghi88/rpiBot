a885f7f2fbdef2071ad6685346526b987f8f2dae
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-11-19 22:29:09 +0700

    try to fix travis, sorry for the spam mails ci deploy

--- Updater/src/RPiBot/Updater/RPiBot.java
+++ Updater/src/RPiBot/Updater/RPiBot.java
@@ -1,4 +1,4 @@
-package RPiBot.Faces;
+package RPiBot.Updater;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -35,3 +35,14 @@ public class RPiBot {
 	}
 
 }
+/*
+Your branch is up-to-date with 'origin/master'.
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git checkout -- <file>..." to discard changes in working directory)
+
+	modified:   pom.xml
+	modified:   ../Updater/src/RPiBot/Faces/RPiBot.java
+
+no changes added to commit (use "git add" and/or "git commit -a")
+*/
