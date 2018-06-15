f9f5add72a8a42f55e526cff16ae53d645afe456
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2018-06-15 08:49:52 -0700

    s

--- THT/TinHocTre/src/tht/FrameMainBFS.java
+++ THT/TinHocTre/src/tht/FrameMainBFS.java
@@ -33,12 +33,12 @@ public class FrameMainBFS extends JFrame {
 	CannyEdgeDetector detector = new CannyEdgeDetector();
 	public int[][] matrix;
 	ArrayList foundEdgeO = new ArrayList<>();
-	ArrayList foundEdgeImgO = new ArrayList<>();
+	ArrayList<BufferedImage> foundEdgeImgO = new ArrayList<>();
 	ArrayList foundEdge = new ArrayList<>();
-	ArrayList foundEdgeImg = new ArrayList<>();
+	ArrayList<BufferedImage> foundEdgeImg = new ArrayList<>();
 	// public int[][] e, e2;
-	int[] rotarray = { 0, 270, 90, 180, 0, 0, 90, 180, 0, 270, 90, 180, 0, 180,180, 0
-	// 0, 270, 90, 180, 0, 270, 90, 180, 0, 270, 90, 180, 180, 180, 180, 0
+	int[] rotarray = { 0, 270, 90, 180, 0, 0, 90, 180, 0, 270, 90, 180, 0, 180, 180, 0
+			// 0, 270, 90, 180, 0, 270, 90, 180, 0, 270, 90, 180, 180, 180, 180, 0
 	};
 	public int[] used;
 	public int[][] res_rot;
@@ -61,8 +61,7 @@ public class FrameMainBFS extends JFrame {
 		// setContentPane(contentP);
 		Container container = getContentPane();
 		container.setLayout(new GridLayout(1, 1));
-		JScrollPane scroll = new JScrollPane(contentP,
-				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+		JScrollPane scroll = new JScrollPane(contentP, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
 				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
 		scroll.setLayout(new ScrollPaneLayout());
@@ -88,8 +87,7 @@ public class FrameMainBFS extends JFrame {
 				// System.out.print(res_marked[i][j] + "*" + res_rot[i][j] +
 				// " ");
 				// System.out.println();
-				BufferedImage im = (BufferedImage) foundEdgeImgO
-						.get(res_marked[i][j] - 1);// ImageIO.read(imageFile);
+				BufferedImage im = (BufferedImage) foundEdgeImgO.get(res_marked[i][j] - 1);// ImageIO.read(imageFile);
 				int w = im.getWidth();
 				int h = im.getHeight();
 				double angle = (Math.PI / 2) * (rotarray[res_rot[i][j]] / 90);
@@ -99,8 +97,7 @@ public class FrameMainBFS extends JFrame {
 				AffineTransform tx = new AffineTransform();
 				tx.rotate(angle, w / 2, h / 2);// (radian,arbit_X,arbit_Y)
 
-				AffineTransformOp op = new AffineTransformOp(tx,
-						AffineTransformOp.TYPE_BILINEAR);
+				AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
 				im = op.filter(im, null);// (sourse,destination)
 				ImageIcon image = new ImageIcon(im);
 				JLabel imageLabel = new JLabel();
@@ -109,8 +106,8 @@ public class FrameMainBFS extends JFrame {
 				// this.getContentPane().setLayout(new GridLayout(3, 3));
 				imageLabel.setLocation(0, 0);
 				imageLabel.setSize(scale, scale);
-				ImageIcon imageIcon = new ImageIcon(image.getImage()
-						.getScaledInstance(scale, scale, Image.SCALE_SMOOTH));
+				ImageIcon imageIcon = new ImageIcon(
+						image.getImage().getScaledInstance(scale, scale, Image.SCALE_SMOOTH));
 				imageLabel.setIcon(imageIcon);
 				imageLabel.setVisible(true);
 				imageLabel.setToolTipText("" + used[res_marked[i][j] - 1]);
@@ -122,8 +119,7 @@ public class FrameMainBFS extends JFrame {
 				// System.out.print(res_marked[i][j] + "*" + res_rot[i][j] +
 				// " ");
 				// System.out.println();
-				BufferedImage im = (BufferedImage) foundEdgeImg
-						.get(res_marked[i][j] - 1);// ImageIO.read(imageFile);
+				BufferedImage im = (BufferedImage) foundEdgeImg.get(res_marked[i][j] - 1);// ImageIO.read(imageFile);
 				int w = im.getWidth();
 				int h = im.getHeight();
 				double angle = (Math.PI / 2) * (rotarray[res_rot[i][j]] / 90);
@@ -133,8 +129,7 @@ public class FrameMainBFS extends JFrame {
 				AffineTransform tx = new AffineTransform();
 				tx.rotate(angle, w / 2, h / 2);// (radian,arbit_X,arbit_Y)
 
-				AffineTransformOp op = new AffineTransformOp(tx,
-						AffineTransformOp.TYPE_BILINEAR);
+				AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
 				im = op.filter(im, null);// (sourse,destination)
 				ImageIcon image = new ImageIcon(im);
 				JLabel imageLabel = new JLabel();
@@ -143,12 +138,66 @@ public class FrameMainBFS extends JFrame {
 				// this.getContentPane().setLayout(new GridLayout(3, 3));
 				imageLabel.setLocation(0, 0);
 				imageLabel.setSize(scale, scale);
-				ImageIcon imageIcon = new ImageIcon(image.getImage()
-						.getScaledInstance(scale, scale, Image.SCALE_SMOOTH));
+				ImageIcon imageIcon = new ImageIcon(
+						image.getImage().getScaledInstance(scale, scale, Image.SCALE_SMOOTH));
 				imageLabel.setIcon(imageIcon);
 				imageLabel.setVisible(true);
 			}
 		}
+	}
+
+    private static int pixelDiff(int rgb1, int rgb2) {
+        int r1 = (rgb1 >> 16) & 0xff;
+        int g1 = (rgb1 >>  8) & 0xff;
+        int b1 =  rgb1        & 0xff;
+        int r2 = (rgb2 >> 16) & 0xff;
+        int g2 = (rgb2 >>  8) & 0xff;
+        int b2 =  rgb2        & 0xff;
+        return Math.abs(r1 - r2) + Math.abs(g1 - g2) + Math.abs(b1 - b2);
+    }
+	public int similarity(BufferedImage im1, BufferedImage im2) {
+		int res = 0;
+		for (int i = 0; i < im1.getWidth(); i++) {
+			for (int j = 0; j < im1.getHeight(); j++) {
+//                res += pixelDiff(im1.getRGB(i, j), im2.getRGB(i, j));
+				if (printPixelARGB(im1.getRGB(i, j)) - printPixelARGB(im2.getRGB(i, j))<1) {
+					res++;
+				}
+			}
+		}
+		res = (int) (((float) res / (im1.getWidth() * im1.getHeight())) * 100);
+		return res;
+//		 long maxDiff = 3L * 255 * im1.getWidth() * im1.getHeight();
+//		 
+//	        return (int) (100.0 * res / maxDiff);
+	}
+
+	public void fillDiff() {
+		for (int i = 0; i < N; i++) {
+			for (int j = i+1; j < N; j++) {
+				if (i != j) {
+					int maxsim = 0;
+					for (int ri = 0; ri < 4; ri++) {
+						BufferedImage im = (BufferedImage) foundEdgeImgO.get(j);
+						int w = im.getWidth();
+						int h = im.getHeight();
+						double angle = (Math.PI / 2) * ri;
+						AffineTransform tx = new AffineTransform();
+						tx.rotate(angle, w / 2, h / 2);
+						AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
+						im = op.filter(im, null);
+						int tmp = similarity(foundEdgeImgO.get(i), im);
+						if(tmp>maxsim) {
+							maxsim=tmp;
+						}
+					}
+					matrix[i][j] = maxsim;
+				}
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+
 	}
 
 	public void init() {
@@ -176,21 +225,21 @@ public class FrameMainBFS extends JFrame {
 		res_marked = new int[R][C];
 		res_rot = new int[R][C];
 		maxi = -9999;
-		matrix = new int[R][C];
-		for (int i = 0; i < R; i++) {
-			for (int j = 0; j < C; j++) {
-				matrix[i][j] = -1;
+		matrix = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				matrix[i][j] = 0;
 			}
 		}
-		matrix[rX - 1][cX - 1] = 1;
 		try {
 			// checkAnImage(new File("D:\\ThiSinh\\input"+numberTest+"\\" + X +
 			// ".txt"));
 			for (int i = 1; i <= N; i++) {
-				checkAnImage(new File("D:\\ThiSinh\\input" + numberTest + "\\"
-						+ i + ".bmp"));
+				checkAnImage(new File("D:\\ThiSinh\\input" + numberTest + "\\" + i + ".bmp"));
 				System.out.println("Processed img " + i);
 			}
+
+			fillDiff();
 			foundpiece = 1;
 			visited[rX - 1][cX - 1] = X;
 			marked[rX - 1][cX - 1] = X;
@@ -203,8 +252,7 @@ public class FrameMainBFS extends JFrame {
 		}
 	}
 
-	public int[] differof1(int[][] e, int[][] e2, int direction, int bound,
-			int rthres, int xttj) {
+	public int[] differof1(int[][] e, int[][] e2, int direction, int bound, int rthres, int xttj) {
 		int[] sum = new int[16];
 		for (int i = 0; i < 16; i++)
 			sum[i] = -1;
@@ -217,18 +265,15 @@ public class FrameMainBFS extends JFrame {
 				continue;
 			if (direction == 1) {
 				if ((e[bound + tt][u] == e2[bound + tt][K - u - 1])
-						&& (e[bound + tt][u + 1] == e2[bound + tt][K - (u + 1)
-								- 1]))
+						&& (e[bound + tt][u + 1] == e2[bound + tt][K - (u + 1) - 1]))
 					sum[0]++;
 				if ((e[bound + tt][u] == e2[K - bound - 1 - tt][u])
 						&& (e[bound + tt][u + 1] == e2[K - bound - 1 - tt][u + 1]))
 					sum[1]++;
-				if ((e[bound + tt][u] == e2[u][bound + tt])
-						&& (e[bound + tt][u + 1] == e2[u + 1][bound + tt]))
+				if ((e[bound + tt][u] == e2[u][bound + tt]) && (e[bound + tt][u + 1] == e2[u + 1][bound + tt]))
 					sum[2]++;
 				if ((e[bound + tt][u] == e2[K - u - 1][K - bound - 1 - tt])
-						&& (e[bound + tt][u + 1] == e2[K - (u + 1) - 1][K
-								- bound - 1 - tt]))
+						&& (e[bound + tt][u + 1] == e2[K - (u + 1) - 1][K - bound - 1 - tt]))
 					sum[3]++;
 			}
 
@@ -236,36 +281,28 @@ public class FrameMainBFS extends JFrame {
 				if ((e[K - bound - 1 - tt][u] == e2[bound + tt][u])
 						&& (e[K - bound - 1 - tt][u + 1] == e2[bound + tt][u + 1]))
 					sum[4]++;
-				if ((e[K - bound - 1 - tt][u] == e2[K - bound - 1 - tt][K - u
-						- 1])
-						&& (e[K - bound - 1 - tt][u + 1] == e2[K - bound - 1
-								- tt][K - (u + 1) - 1]))
+				if ((e[K - bound - 1 - tt][u] == e2[K - bound - 1 - tt][K - u - 1])
+						&& (e[K - bound - 1 - tt][u + 1] == e2[K - bound - 1 - tt][K - (u + 1) - 1]))
 					sum[5]++;
 				if ((e[K - bound - 1 - tt][u] == e2[K - u - 1][bound + tt])
-						&& (e[K - bound - 1 - tt][u + 1] == e2[K - (u + 1) - 1][bound
-								+ tt]))
+						&& (e[K - bound - 1 - tt][u + 1] == e2[K - (u + 1) - 1][bound + tt]))
 					sum[6]++;
 				if ((e[K - bound - 1 - tt][u] == e2[u][K - bound - 1 - tt])
-						&& (e[K - bound - 1 - tt][u + 1] == e2[u + 1][K - bound
-								- 1 - tt]))
+						&& (e[K - bound - 1 - tt][u + 1] == e2[u + 1][K - bound - 1 - tt]))
 					sum[7]++;
 			}
 
 			if (direction == 3) {
-				if ((e[u][bound + tt] == e2[bound + tt][u])
-						&& (e[u + 1][bound + tt] == e2[bound + tt][u + 1]))
+				if ((e[u][bound + tt] == e2[bound + tt][u]) && (e[u + 1][bound + tt] == e2[bound + tt][u + 1]))
 					sum[8]++;
 				if ((e[u][bound + tt] == e2[K - bound - 1 - tt][K - u - 1])
-						&& (e[u + 1][bound + tt] == e2[K - bound - 1 - tt][K
-								- (u + 1) - 1]))
+						&& (e[u + 1][bound + tt] == e2[K - bound - 1 - tt][K - (u + 1) - 1]))
 					sum[9]++;
 				if ((e[u][bound + tt] == e2[K - u - 1][bound + tt])
-						&& (e[u + 1][bound + tt] == e2[K - (u + 1) - 1][bound
-								+ tt]))
+						&& (e[u + 1][bound + tt] == e2[K - (u + 1) - 1][bound + tt]))
 					sum[10]++;
 				if ((e[u][bound + tt] == e2[u][K - bound - 1 - tt])
-						&& (e[u + 1][bound + tt] == e2[u + 1][K - bound - 1
-								- tt]))
+						&& (e[u + 1][bound + tt] == e2[u + 1][K - bound - 1 - tt]))
 					sum[11]++;
 			}
 
@@ -273,18 +310,14 @@ public class FrameMainBFS extends JFrame {
 				if ((e[u][K - bound - 1 - tt] == e2[bound + tt][u])
 						&& (e[u + 1][K - bound - 1 - tt] == e2[bound + tt][u + 1]))
 					sum[12]++;
-				if ((e[u][K - bound - 1 - tt] == e2[K - bound - 1 - tt][K - u
-						- 1])
-						&& (e[u + 1][K - bound - 1 - tt] == e2[K - bound - 1
-								- tt][K - (u + 1) - 1]))
+				if ((e[u][K - bound - 1 - tt] == e2[K - bound - 1 - tt][K - u - 1])
+						&& (e[u + 1][K - bound - 1 - tt] == e2[K - bound - 1 - tt][K - (u + 1) - 1]))
 					sum[13]++;
 				if ((e[u][K - bound - 1 - tt] == e2[K - u - 1][bound + tt])
-						&& (e[u + 1][K - bound - 1 - tt] == e2[K - (u + 1) - 1][bound
-								+ tt]))
+						&& (e[u + 1][K - bound - 1 - tt] == e2[K - (u + 1) - 1][bound + tt]))
 					sum[14]++;
 				if ((e[u][K - bound - 1 - tt] == e2[u][K - bound - 1 - tt])
-						&& (e[u + 1][K - bound - 1 - tt] == e2[u + 1][K - bound
-								- 1 - tt]))
+						&& (e[u + 1][K - bound - 1 - tt] == e2[u + 1][K - bound - 1 - tt]))
 					sum[15]++;
 			}
 		}
@@ -302,8 +335,7 @@ public class FrameMainBFS extends JFrame {
 		return new int[] { ff, r };
 	}
 
-	public int[] differof2(int[][] e, int[][] e2, int direction, int bound,
-			int rthres, int xtt) {
+	public int[] differof2(int[][] e, int[][] e2, int direction, int bound, int rthres, int xtt) {
 		int[] sum = new int[16];
 		for (int i = 0; i < 16; i++)
 			sum[i] = -1;
@@ -320,30 +352,25 @@ public class FrameMainBFS extends JFrame {
 						sum[1]++;
 					if (Math.abs(e[bound + tt][u] - e2[u][bound + tt]) >= rthres)
 						sum[2]++;
-					if (Math.abs(e[bound + tt][u]
-							- e2[K - u - 1][K - bound - 1 - tt]) >= rthres)
+					if (Math.abs(e[bound + tt][u] - e2[K - u - 1][K - bound - 1 - tt]) >= rthres)
 						sum[3]++;
 				}
 
 				if (direction == 0) {
 					if (Math.abs(e[K - bound - 1 - tt][u] - e2[bound + tt][u]) >= rthres)
 						sum[4]++;
-					if (Math.abs(e[K - bound - 1 - tt][u]
-							- e2[K - bound - 1 - tt][K - u - 1]) >= rthres)
+					if (Math.abs(e[K - bound - 1 - tt][u] - e2[K - bound - 1 - tt][K - u - 1]) >= rthres)
 						sum[5]++;
-					if (Math.abs(e[K - bound - 1 - tt][u]
-							- e2[K - u - 1][bound + tt]) >= rthres)
+					if (Math.abs(e[K - bound - 1 - tt][u] - e2[K - u - 1][bound + tt]) >= rthres)
 						sum[6]++;
-					if (Math.abs(e[K - bound - 1 - tt][u]
-							- e2[u][K - bound - 1 - tt]) >= rthres)
+					if (Math.abs(e[K - bound - 1 - tt][u] - e2[u][K - bound - 1 - tt]) >= rthres)
 						sum[7]++;
 				}
 
 				if (direction == 3) {
 					if (Math.abs(e[u][bound + tt] - e2[bound + tt][u]) >= rthres)
 						sum[8]++;
-					if (Math.abs(e[u][bound + tt]
-							- e2[K - bound - 1 - tt][K - u - 1]) >= rthres)
+					if (Math.abs(e[u][bound + tt] - e2[K - bound - 1 - tt][K - u - 1]) >= rthres)
 						sum[9]++;
 					if (Math.abs(e[u][bound + tt] - e2[K - u - 1][bound + tt]) >= rthres)
 						sum[10]++;
@@ -354,14 +381,11 @@ public class FrameMainBFS extends JFrame {
 				if (direction == 2) {
 					if (Math.abs(e[u][K - bound - 1 - tt] - e2[bound + tt][u]) >= rthres)
 						sum[12]++;
-					if (Math.abs(e[u][K - bound - 1 - tt]
-							- e2[K - bound - 1 - tt][K - u - 1]) >= rthres)
+					if (Math.abs(e[u][K - bound - 1 - tt] - e2[K - bound - 1 - tt][K - u - 1]) >= rthres)
 						sum[13]++;
-					if (Math.abs(e[u][K - bound - 1 - tt]
-							- e2[K - u - 1][bound + tt]) >= rthres)
+					if (Math.abs(e[u][K - bound - 1 - tt] - e2[K - u - 1][bound + tt]) >= rthres)
 						sum[14]++;
-					if (Math.abs(e[u][K - bound - 1 - tt]
-							- e2[u][K - bound - 1 - tt]) >= rthres)
+					if (Math.abs(e[u][K - bound - 1 - tt] - e2[u][K - bound - 1 - tt]) >= rthres)
 						sum[15]++;
 				}
 			}
@@ -394,8 +418,8 @@ public class FrameMainBFS extends JFrame {
 				}
 				for (int i = 0; i < R; i++) {
 					for (int j = 0; j < C; j++)
-						System.out.print(res_marked[i][j] + "*" + res_rot[i][j]
-								+ "\t" + used[res_marked[i][j] - 1] + "\t");
+						System.out.print(
+								res_marked[i][j] + "*" + res_rot[i][j] + "\t" + used[res_marked[i][j] - 1] + "\t");
 					System.out.println();
 				}
 				// for (int i = 0; i < N; i++)
@@ -441,14 +465,13 @@ public class FrameMainBFS extends JFrame {
 		int[] mintmp = { 9999999, 9999999, 9999999, 9999999 };
 		int[] maxtmp = { -9999999, -9999999, -9999999, -9999999 };
 		for (int i = 0; i < 4; i++) {
-			if (xp + dx[i] >= 0 && xp + dx[i] < R && yp + dy[i] >= 0
-					&& yp + dy[i] < C) {
+			if (xp + dx[i] >= 0 && xp + dx[i] < R && yp + dy[i] >= 0 && yp + dy[i] < C) {
 				if (marked[xp + dx[i]][yp + dy[i]] == 0) {
 					// int mx = -1, xxx = -1, rr = 0;
 					int jjj = 0, rott = 0;
 					mintmp[i] = 9999999;
 					for (int j = 0; j < N; j++) {
-						if (j == theX || used[j] > 0)
+						if (j == theX || used[j] > 0 ||  matrix[theX][j]>40||  matrix[j][theX]>40)
 							continue;
 						int[][] e2 = (int[][]) foundEdge.get(j);
 						int[][] e2O = (int[][]) foundEdgeO.get(j);
@@ -462,19 +485,14 @@ public class FrameMainBFS extends JFrame {
 							int xxp = xp + dx[i] + dx[ii];
 							int yyp = yp + dy[i] + dy[ii];
 							if (xxp >= 0 && xxp < R && yyp >= 0 && yyp < C) {
-								if (marked[xxp][yyp] > 0
-										&& marked[xxp][yyp] != theX + 1) {
-									int[] tmp2 = differof2(e2,
-											(int[][]) foundEdge
-													.get(marked[xxp][yyp] - 1),
-											ii, 7, 0, 1);// 7, 1, 15
+								if (marked[xxp][yyp] > 0 && marked[xxp][yyp] != theX + 1) {
+									int[] tmp2 = differof2(e2, (int[][]) foundEdge.get(marked[xxp][yyp] - 1), ii, 7, 0,
+											1);// 7, 1, 15
 									// int[] tmp3 = differof1(e2, (int[][])
 									// foundEdge.get(marked[xxp][yyp] - 1), ii,
 									// 7, 2);
-									int[] tmp2O = differof2(e2O,
-											(int[][]) foundEdgeO
-													.get(marked[xxp][yyp] - 1),
-											ii, 0, 2, 1);// 0,50, 15
+									int[] tmp2O = differof2(e2O, (int[][]) foundEdgeO.get(marked[xxp][yyp] - 1), ii, 0,
+											2, 1);// 0,50, 15
 									// int[] tmp2O1= differof1(e2O, (int[][])
 									// foundEdgeO.get(marked[xxp][yyp] - 1),
 									// ii, 0,5);
@@ -551,8 +569,7 @@ public class FrameMainBFS extends JFrame {
 		detector.setSourceImage(bufferImage2);
 		detector.process();
 		BufferedImage edges = detector.getEdgesImage();
-		BufferedImage b = new BufferedImage(edges.getWidth(),
-				edges.getHeight(), edges.getType());
+		BufferedImage b = new BufferedImage(edges.getWidth(), edges.getHeight(), edges.getType());
 		Graphics g = b.getGraphics();
 		g.drawImage(edges, 0, 0, null);
 		g.dispose();
@@ -617,8 +634,7 @@ public class FrameMainBFS extends JFrame {
 	public void readInfo() {
 		Scanner scanner;
 		try {
-			scanner = new Scanner(new File("D:\\ThiSinh\\input" + numberTest
-					+ "\\info.txt"));
+			scanner = new Scanner(new File("D:\\ThiSinh\\input" + numberTest + "\\info.txt"));
 			int[] t = new int[100];
 			int i = 0;
 			while (scanner.hasNextInt()) {
@@ -649,17 +665,13 @@ public class FrameMainBFS extends JFrame {
 
 	}
 
-	static public int[][] create2DIntMatrixFromFile(Path path)
-			throws IOException {
-		return Files
-				.lines(path)
-				.map((l) -> l.trim().split("\\s+"))
-				.map((sa) -> Stream.of(sa).mapToInt(Integer::parseInt)
-						.toArray()).toArray(int[][]::new);
+	static public int[][] create2DIntMatrixFromFile(Path path) throws IOException {
+		return Files.lines(path).map((l) -> l.trim().split("\\s+"))
+				.map((sa) -> Stream.of(sa).mapToInt(Integer::parseInt).toArray()).toArray(int[][]::new);
 	}
 
 	public static void main(String args[]) {
-		int i = 9;
+		int i = 0;
 		// for (i = 15; i < 26; i++) {
 		FrameMainBFS f = new FrameMainBFS();
 		// if(i==15)
